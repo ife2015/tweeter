7bf1a61f050fd06706839db99a6eb1c12d1b7a96
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-08 23:06:57 +0000

    added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
+// function tracks the number of characters in text area
 $(document).ready(function() {
-  $('#tweet-text').on('keyup',function(event) {
-    if(this.value.length <= 140) {  
-      $('.counter').css('color','#545149');
+  $('#tweet-text').on('keyup', function(event) {
+    if (this.value.length <= 140) {
+      $('.counter').css('color', '#545149');
       const valueChange = 140 - this.value.length;
       $('.counter').text(valueChange);
     } else {
-      $('.counter').css('color','red');
+      $('.counter').css('color', 'red');
       const negValChange = 140 - this.value.length;
       $('.counter').text(negValChange);
     }
-  })
-})
+  });
+});
