dad1a0add42968f767b0fe9bc81d4c60dc25a23c
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-08 03:42:10 +0000

    included coloring on original counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#tweet-text').on('keyup',function(event) {
     if(this.value.length <= 140) {  
+      $('.counter').css('color','#545149');
       const valueChange = 140 - this.value.length;
       $('.counter').text(valueChange);
     } else {
