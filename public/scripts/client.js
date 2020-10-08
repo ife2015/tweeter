2b0bea844a8e76b2e8025d7df1e4c9e815f8658a
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-08 22:06:08 +0000

    removed comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,35 +5,9 @@
  */
 
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": Math.floor(((new Date()).getTime() - 1461116232227)/(1000*3600*24))
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-
 const createTweetElement = function(tweetObject) {
 
-  const escape =  function(str) {
+  const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
@@ -63,7 +37,7 @@ const createTweetElement = function(tweetObject) {
 };
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
   const renderTweets = function(tweets) {
     $('.tweets-container').empty();
@@ -82,25 +56,20 @@ $(document).ready(function () {
   $form.on("submit", function(event) {
     event.preventDefault();
     if (+$('.counter').val() < 0) {
-      $('.error-message').text("⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️ Max characters exceeded!");
+      $('.error-message').text("⚠️⚠️⚠️⚠️ Max characters exceeded! ⚠️⚠️⚠️⚠️");
       $('.error-message').show(5000);
     } else if (!$('#tweet-text').val()) {
-      $('.error-message').text("⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️ Please Hum Something! ERROR101")
+      $('.error-message').text("⚠️⚠️⚠️⚠️ Please Hum Something! ⚠️⚠️⚠️⚠️")
       $('.error-message').show(1000);
     } else {
       $.post('/tweets', $(this).serialize())
-        .then(function () {
-          loadTweets()
+        .then(function() {
+          loadTweets();
         })
     }
   });
-  
+
   loadTweets();
 });
 
 
-// The user should be given an error that their tweet content is too 
-// long or that it is not present 
-// (ideally separate messages for each scenario)
-// The form should not be cleared
-// The form should not submit
