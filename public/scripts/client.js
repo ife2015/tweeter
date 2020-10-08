192e44dd85f11ba767edf66e4799e5061c906e8e
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-08 23:16:30 +0000

    added functionality to clear text-area

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,14 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
+//extracts infomation to create new tweet posts
 const createTweetElement = function(tweetObject) {
-
+  
+  //preventing XSS with Escaping
   const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
   let $tweet = `
   <article class="tweet-box">
@@ -24,52 +25,58 @@ const createTweetElement = function(tweetObject) {
     </header>
     <p class="post-text">${escape(tweetObject.content.text)}</p>
     <footer class="post-footer">
-      <span> ${Math.floor(((new Date()).getTime() - tweetObject.created_at)/(8640000))} days ago</span>
+      <span> ${Math.floor(((new Date()).getTime() - tweetObject.created_at) / (8640000))} days ago</span>
       <span class="icons">
         <i class="fas fa-heart"></i>
         <i class="fas fa-retweet"></i>
         <i class="fas fa-flag"></i>
       </span>
     </footer>
-  </article>`
+  </article>`;
 
   return $tweet;
 };
 
 
 $(document).ready(function() {
-
+ 
+  // displays new tweets to page
   const renderTweets = function(tweets) {
     $('.tweets-container').empty();
     tweets.reverse().forEach(tweet => {
       $('.tweets-container').append(createTweetElement(tweet));
     });
-  }
-
+  };
+  
+  
   const loadTweets = function() {
     $('.error-message').empty();
     $.getJSON('/tweets')
-      .then(renderTweets)
-  }
-
+    .then(renderTweets);
+  };
+  
   const $form = $('.tweet-form');
   $form.on("submit", function(event) {
+    // prevents form from refreshing page
     event.preventDefault();
+    // validates characters in text area and length not exceeded
     if (+$('.counter').val() < 0) {
       $('.error-message').text("⚠️⚠️⚠️⚠️ Max characters exceeded! ⚠️⚠️⚠️⚠️");
       $('.error-message').show(5000);
     } else if (!$('#tweet-text').val()) {
-      $('.error-message').text("⚠️⚠️⚠️⚠️ Please Hum Something! ⚠️⚠️⚠️⚠️")
+      $('.error-message').text("⚠️⚠️⚠️⚠️ Please Hum Something! ⚠️⚠️⚠️⚠️");
       $('.error-message').show(1000);
     } else {
       $.post('/tweets', $(this).serialize())
-        .then(function() {
-          loadTweets();
-        })
+      .then(function() {
+        $('#tweet-text').val("");
+        loadTweets();
+      });
     }
   });
 
   loadTweets();
+
 });
 
 
